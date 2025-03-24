a707e06f40ad97a9206f6dcabb8e38b1c9856121
KhanhTQ-hub <khanhquang0912qh@gmail.com>
2025-03-24 15:30:00 +0700

    fix: release datetime package

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("com.ktgame.datetime")]
+[assembly: AssemblyProduct("com.ktgame.datetime")]
---
+++ Runtime/IDateTime.cs
@@ -0,0 +1,9 @@
+﻿namespace com.ktgame.datetime
+{
+    public interface IDateTime
+    {
+        System.DateTime Now { get; }
+        System.DateTime NowUtc { get; }
+        UnbiasedTime UnbiasedTime { get; }
+    }
+}
